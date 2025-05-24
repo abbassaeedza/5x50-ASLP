f735152821b78c3e5a3ccf5dd6db0af243a3dde6
Abbas Saeed Zaidi <abbassaeed.za@gmail.com>
2025-05-24 19:27:28 +0500

    redesign algorithm

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,13 @@
 
 using namespace std;
 
-const int LINES = 5;
-const int JOBS = 50;
+const int LINES = 5; // lines
+const int JOBS = 50; // processingCosts
 
-int entryCosts[LINES];
-int exitCosts[LINES];
-int processingCosts[LINES][JOBS];
-int transferCosts[LINES][LINES][JOBS];
+int entryCosts[LINES];                 // E(line)
+int exitCosts[LINES];                  // X(line)
+int processingCosts[LINES][JOBS];      // A(line, station)
+int transferCosts[LINES][LINES][JOBS]; // T(lineFrom, lineTo, station)
 
 string splitEnd(string str, string delim)
 {
@@ -39,7 +39,6 @@ void getEntryExitCosts()
         getline(inputString, tempStr, ',');
         temp = splitEnd(tempStr, "_");
         row = atoi(temp.c_str()) - 1;
-
         // ENTRIES
         getline(inputString, tempStr, ',');
         entryCosts[row] = atoi(tempStr.c_str());
@@ -73,6 +72,7 @@ void getProcessingCosts()
         temp = splitEnd(tempStr, "_");
         job = atoi(temp.c_str()) - 1;
 
+        // PROCESSING COST
         for (int i = 0; i < LINES; i++)
         {
             getline(inputString, tempStr, ',');
@@ -122,6 +122,7 @@ void getTransferCosts()
         temp = splitEnd(tempStr, "_");
         cost = atoi(temp.c_str());
 
+        // TRANSFER COST
         transferCosts[linefrom][lineto][job] = cost;
     }
 
@@ -141,242 +142,301 @@ void getTransferCosts()
     }
 }
 
-// int(*stationCost())[JOBS]
-//{
-//	static int stations[LINES][JOBS];
-//	for (int i = 0; i < LINES; i++)
-//	{
-//		for (int j = 0; j < JOBS; j++)
-//		{
-//			stations[i][j] = BASE_COST + processingCosts[i][j] + (FAILURE_PENALTY * FAILURE[i][j]);
-//			cout << stations[i][j] << " ";
-//		}
-//		cout << endl;
-//	}
-//
-//	return stations;
-// }
-//
-// int fastestWay(int stations[][JOBS])
-//{
-//	int f[LINES][JOBS + 1];
-//	int l[LINES][JOBS + 1];
-//
-//	f[0][JOBS] = 0;
-//	f[1][JOBS] = 0;
-//	f[2][JOBS] = 0;
-//	f[3][JOBS] = 0;
-//	l[0][JOBS] = 0;
-//	l[1][JOBS] = 0;
-//	l[2][JOBS] = 0;
-//	l[3][JOBS] = 0;
-//
-//	for (int i = 0; i < LINES; i++)
-//	{
-//		f[i][0] = entryCosts[i] + stations[i][0];
-//		l[i][0] = i + 1;
-//	}
-//
-//	for (int j = 1; j < JOBS; j++)
-//	{
-//		if (f[0][j - 1] + stations[0][j] <=
-//			min(f[1][j - 1] + TRANSFER_COST + stations[0][j],
-//				min(f[2][j - 1] + TRANSFER_COST + stations[0][j],
-//					f[3][j - 1] + TRANSFER_COST + stations[0][j])))
-//		{
-//			f[0][j] = f[0][j - 1] + stations[0][j];
-//			l[0][j] = 1;
-//		}
-//		else if (f[1][j - 1] + TRANSFER_COST + stations[0][j] <=
-//			min(f[2][j - 1] + TRANSFER_COST + stations[0][j],
-//				f[3][j - 1] + TRANSFER_COST + stations[0][j]))
-//		{
-//			f[0][j] = f[1][j - 1] + TRANSFER_COST + stations[0][j];
-//			l[0][j] = 2;
-//		}
-//		else if (f[2][j - 1] + TRANSFER_COST + stations[0][j] <=
-//			f[3][j - 1] + TRANSFER_COST + stations[0][j])
-//		{
-//			f[0][j] = f[2][j - 1] + TRANSFER_COST + stations[0][j];
-//			l[0][j] = 3;
-//		}
-//		else
-//		{
-//			f[0][j] = f[3][j - 1] + TRANSFER_COST + stations[0][j];
-//			l[0][j] = 4;
-//		}
-//
-//		if (f[1][j - 1] + stations[1][j] <=
-//			min(f[0][j - 1] + TRANSFER_COST + stations[1][j],
-//				min(f[2][j - 1] + TRANSFER_COST + stations[1][j],
-//					f[3][j - 1] + TRANSFER_COST + stations[1][j])))
-//		{
-//			f[1][j] = f[1][j - 1] + stations[1][j];
-//			l[1][j] = 2;
-//		}
-//		else if (f[0][j - 1] + TRANSFER_COST + stations[1][j] <=
-//			min(f[2][j - 1] + TRANSFER_COST + stations[1][j],
-//				f[3][j - 1] + TRANSFER_COST + stations[1][j]))
-//		{
-//			f[1][j] = f[0][j - 1] + TRANSFER_COST + stations[1][j];
-//			l[1][j] = 1;
-//		}
-//		else if (f[2][j - 1] + TRANSFER_COST + stations[1][j] <=
-//			f[3][j - 1] + TRANSFER_COST + stations[1][j])
-//		{
-//			f[1][j] = f[2][j - 1] + TRANSFER_COST + stations[1][j];
-//			l[1][j] = 3;
-//		}
-//		else
-//		{
-//			f[1][j] = f[3][j - 1] + TRANSFER_COST + stations[1][j];
-//			l[1][j] = 4;
-//		}
-//
-//		if (f[2][j - 1] + stations[2][j] <=
-//			min(f[0][j - 1] + TRANSFER_COST + stations[2][j],
-//				min(f[1][j - 1] + TRANSFER_COST + stations[2][j],
-//					f[3][j - 1] + TRANSFER_COST + stations[2][j])))
-//		{
-//			f[2][j] = f[2][j - 1] + stations[2][j];
-//			l[2][j] = 3;
-//		}
-//		else if (f[0][j - 1] + TRANSFER_COST + stations[2][j] <=
-//			min(f[1][j - 1] + TRANSFER_COST + stations[2][j],
-//				f[3][j - 1] + TRANSFER_COST + stations[2][j]))
-//		{
-//			f[2][j] = f[0][j - 1] + TRANSFER_COST + stations[2][j];
-//			l[2][j] = 1;
-//		}
-//		else if (f[1][j - 1] + TRANSFER_COST + stations[2][j] <=
-//			f[3][j - 1] + TRANSFER_COST + stations[2][j])
-//		{
-//			f[2][j] = f[1][j - 1] + TRANSFER_COST + stations[2][j];
-//			l[2][j] = 2;
-//		}
-//		else
-//		{
-//			f[2][j] = f[3][j - 1] + TRANSFER_COST + stations[2][j];
-//			l[2][j] = 4;
-//		}
-//
-//		if (f[3][j - 1] + stations[3][j] <=
-//			min(f[0][j - 1] + TRANSFER_COST + stations[3][j],
-//				min(f[1][j - 1] + TRANSFER_COST + stations[3][j],
-//					f[2][j - 1] + TRANSFER_COST + stations[3][j])))
-//		{
-//			f[3][j] = f[3][j - 1] + stations[3][j];
-//			l[3][j] = 4;
-//		}
-//		else if (f[0][j - 1] + TRANSFER_COST + stations[3][j] <=
-//			min(f[1][j - 1] + TRANSFER_COST + stations[3][j],
-//				f[2][j - 1] + TRANSFER_COST + stations[3][j]))
-//		{
-//			f[3][j] = f[0][j - 1] + TRANSFER_COST + stations[3][j];
-//			l[3][j] = 1;
-//		}
-//		else if (f[1][j - 1] + TRANSFER_COST + stations[3][j] <=
-//			f[2][j - 1] + TRANSFER_COST + stations[3][j])
-//		{
-//			f[3][j] = f[1][j - 1] + TRANSFER_COST + stations[3][j];
-//			l[3][j] = 2;
-//		}
-//		else
-//		{
-//			f[3][j] = f[2][j - 1] + TRANSFER_COST + stations[3][j];
-//			l[3][j] = 3;
-//		}
-//
-//		if (f[3][j - 1] + stations[3][j] <=
-//			min(f[0][j - 1] + TRANSFER_COST + stations[3][j],
-//				min(f[1][j - 1] + TRANSFER_COST + stations[3][j],
-//					f[2][j - 1] + TRANSFER_COST + stations[3][j])))
-//		{
-//			f[3][j] = f[3][j - 1] + stations[3][j];
-//			l[3][j] = 4;
-//		}
-//		else if (f[0][j - 1] + TRANSFER_COST + stations[3][j] <=
-//			min(f[1][j - 1] + TRANSFER_COST + stations[3][j],
-//				f[2][j - 1] + TRANSFER_COST + stations[3][j]))
-//		{
-//			f[3][j] = f[0][j - 1] + TRANSFER_COST + stations[3][j];
-//			l[3][j] = 1;
-//		}
-//		else if (f[1][j - 1] + TRANSFER_COST + stations[3][j] <=
-//			f[2][j - 1] + TRANSFER_COST + stations[3][j])
-//		{
-//			f[3][j] = f[1][j - 1] + TRANSFER_COST + stations[3][j];
-//			l[3][j] = 2;
-//		}
-//		else
-//		{
-//			f[3][j] = f[2][j - 1] + TRANSFER_COST + stations[3][j];
-//			l[3][j] = 3;
-//		}
-//	}
-//
-//	if (f[0][JOBS - 1] + exitCosts[0] <=
-//		min(f[1][JOBS - 1] + exitCosts[1],
-//			min(f[2][JOBS - 1] + exitCosts[2],
-//				f[3][JOBS - 1] + exitCosts[3])))
-//	{
-//		f[0][JOBS] = f[0][JOBS - 1] + exitCosts[0];
-//		l[0][JOBS] = 1;
-//	}
-//	else if (f[1][JOBS - 1] + exitCosts[1] <=
-//		min(f[2][JOBS - 1] + exitCosts[2],
-//			f[3][JOBS - 1] + exitCosts[3]))
-//	{
-//		f[1][JOBS] = f[1][JOBS - 1] + exitCosts[1];
-//		l[1][JOBS] = 2;
-//	}
-//	else if (f[2][JOBS - 1] + exitCosts[2] <=
-//		f[3][JOBS - 1] + exitCosts[3])
-//	{
-//		f[2][JOBS] = f[2][JOBS - 1] + exitCosts[2];
-//		l[2][JOBS] = 3;
-//	}
-//	else
-//	{
-//		f[3][JOBS] = f[3][JOBS - 1] + exitCosts[3];
-//		l[3][JOBS] = 4;
-//	}
-//
-//	for (int i = 0; i < LINES; i++)
-//	{
-//		for (int j = 0; j <= JOBS; j++)
-//		{
-//			cout << f[i][j] << "\t";
-//		}
-//		cout << endl;
-//	}
-//	cout << endl;
-//	for (int i = 0; i < LINES; i++)
-//	{
-//		for (int j = 0; j <= JOBS; j++)
-//		{
-//			cout << l[i][j] << " ";
-//		}
-//		cout << endl;
-//	}
-//
-//	return 0;
-// }
+/*int(*stationCost())[JOBS]
+{
+    static int stations[LINES][JOBS];
+    for (int i = 0; i < LINES; i++)
+    {
+        for (int j = 0; j < JOBS; j++)
+        {
+            stations[i][j] = BASE_COST + processingCosts[i][j] + (FAILURE_PENALTY * FAILURE[i][j]);
+            cout << stations[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return stations;
+ }*/
+
+void fastestWay()
+{
+    int f[LINES][JOBS + 1];
+    int l[LINES][JOBS + 1];
+
+    for (int i = 0; i < LINES; i++)
+    {
+        f[i][JOBS] = 0;
+        l[i][JOBS] = 0;
+    }
+
+    // f[0][JOBS] = 0;
+    // f[1][JOBS] = 0;
+    // f[2][JOBS] = 0;
+    // f[3][JOBS] = 0;
+    // f[4][JOBS] = 0;
+    // l[0][JOBS] = 0;
+    // l[1][JOBS] = 0;
+    // l[2][JOBS] = 0;
+    // l[3][JOBS] = 0;
+    // l[4][JOBS] = 0;
+
+    // F[LINES][1] and L[LINES][1]
+    for (int i = 0; i < LINES; i++)
+    {
+        f[i][0] = entryCosts[i] + processingCosts[i][0];
+        l[i][0] = i + 1;
+    }
+
+    for (int j = 1; j < JOBS; j++)
+    {
+        // LINE = 1
+        if (f[0][j - 1] + processingCosts[0][j] <=
+            min(f[1][j - 1] + transferCosts[1][0][j - 1] + processingCosts[0][j],
+                min(f[2][j - 1] + transferCosts[2][0][j - 1] + processingCosts[0][j],
+                    min(f[3][j - 1] + transferCosts[3][0][j - 1] + processingCosts[0][j],
+                        f[4][j - 1] + transferCosts[4][0][j - 1] + processingCosts[0][j]))))
+        {
+            f[0][j] = f[0][j - 1] + processingCosts[0][j];
+            l[0][j] = 1;
+        }
+        else if (f[1][j - 1] + transferCosts[1][0][j - 1] + processingCosts[0][j] <=
+                 min(f[2][j - 1] + transferCosts[2][0][j - 1] + processingCosts[0][j],
+                     min(f[3][j - 1] + transferCosts[3][0][j - 1] + processingCosts[0][j],
+                         f[4][j - 1] + transferCosts[4][0][j - 1] + processingCosts[0][j])))
+        {
+            f[0][j] = f[1][j - 1] + transferCosts[1][0][j - 1] + processingCosts[0][j];
+            l[0][j] = 2;
+        }
+        else if (f[2][j - 1] + transferCosts[2][0][j - 1] + processingCosts[0][j] <=
+                 min(f[3][j - 1] + transferCosts[3][0][j - 1] + processingCosts[0][j],
+                     f[4][j - 1] + transferCosts[4][0][j - 1] + processingCosts[0][j]))
+        {
+            f[0][j] = f[2][j - 1] + transferCosts[2][0][j - 1] + processingCosts[0][j];
+            l[0][j] = 3;
+        }
+        else if (f[3][j - 1] + transferCosts[3][0][j - 1] + processingCosts[0][j] <=
+                 f[4][j - 1] + transferCosts[4][0][j - 1] + processingCosts[0][j])
+        {
+            f[0][j] = f[3][j - 1] + transferCosts[3][0][j - 1] + processingCosts[0][j];
+            l[0][j] = 4;
+        }
+        else
+        {
+            f[0][j] = f[4][j - 1] + transferCosts[4][0][j - 1] + processingCosts[0][j];
+            l[0][j] = 5;
+        }
+
+        // LINE = 2
+        if (f[1][j - 1] + processingCosts[1][j] <=
+            min(f[0][j - 1] + transferCosts[0][1][j - 1] + processingCosts[1][j],
+                min(f[2][j - 1] + transferCosts[2][1][j - 1] + processingCosts[1][j],
+                    min(f[3][j - 1] + transferCosts[3][1][j - 1] + processingCosts[1][j],
+                        f[4][j - 1] + transferCosts[4][1][j - 1] + processingCosts[1][j]))))
+        {
+            f[1][j] = f[1][j - 1] + processingCosts[1][j];
+            l[1][j] = 2;
+        }
+        else if (f[0][j - 1] + transferCosts[0][1][j - 1] + processingCosts[1][j] <=
+                 min(f[2][j - 1] + transferCosts[2][1][j - 1] + processingCosts[1][j],
+                     min(f[3][j - 1] + transferCosts[3][1][j - 1] + processingCosts[1][j],
+                         f[4][j - 1] + transferCosts[4][1][j - 1] + processingCosts[1][j])))
+        {
+            f[1][j] = f[0][j - 1] + transferCosts[0][1][j - 1] + processingCosts[1][j];
+            l[1][j] = 1;
+        }
+        else if (f[2][j - 1] + transferCosts[2][1][j - 1] + processingCosts[1][j] <=
+                 min(f[3][j - 1] + transferCosts[3][1][j - 1] + processingCosts[1][j],
+                     f[4][j - 1] + transferCosts[4][1][j - 1] + processingCosts[1][j]))
+        {
+            f[1][j] = f[2][j - 1] + transferCosts[2][1][j - 1] + processingCosts[1][j];
+            l[1][j] = 3;
+        }
+        else if (f[3][j - 1] + transferCosts[3][1][j - 1] + processingCosts[1][j] <=
+                 f[4][j - 1] + transferCosts[4][1][j - 1] + processingCosts[1][j])
+        {
+            f[1][j] = f[3][j - 1] + transferCosts[3][1][j - 1] + processingCosts[1][j];
+            l[1][j] = 4;
+        }
+        else
+        {
+            f[1][j] = f[4][j - 1] + transferCosts[4][1][j - 1] + processingCosts[1][j];
+            l[1][j] = 5;
+        }
+
+        // LINE = 3
+        if (f[2][j - 1] + processingCosts[2][j] <=
+            min(f[0][j - 1] + transferCosts[0][2][j - 1] + processingCosts[2][j],
+                min(f[1][j - 1] + transferCosts[1][2][j - 1] + processingCosts[2][j],
+                    min(f[3][j - 1] + transferCosts[3][2][j - 1] + processingCosts[2][j],
+                        f[4][j - 1] + transferCosts[4][2][j - 1] + processingCosts[2][j]))))
+        {
+            f[2][j] = f[2][j - 1] + processingCosts[2][j];
+            l[2][j] = 3;
+        }
+        else if (f[0][j - 1] + transferCosts[0][2][j - 1] + processingCosts[2][j] <=
+                 min(f[1][j - 1] + transferCosts[1][2][j - 1] + processingCosts[2][j],
+                     min(f[3][j - 1] + transferCosts[3][2][j - 1] + processingCosts[2][j],
+                         f[4][j - 1] + transferCosts[4][2][j - 1] + processingCosts[2][j])))
+        {
+            f[2][j] = f[0][j - 1] + transferCosts[0][2][j - 1] + processingCosts[2][j];
+            l[2][j] = 1;
+        }
+        else if (f[1][j - 1] + transferCosts[1][2][j - 1] + processingCosts[2][j] <=
+                 min(f[3][j - 1] + transferCosts[3][2][j - 1] + processingCosts[2][j],
+                     f[4][j - 1] + transferCosts[4][2][j - 1] + processingCosts[2][j]))
+        {
+            f[2][j] = f[1][j - 1] + transferCosts[1][2][j - 1] + processingCosts[2][j];
+            l[2][j] = 2;
+        }
+        else if (f[3][j - 1] + transferCosts[3][2][j - 1] + processingCosts[2][j] <=
+                 f[4][j - 1] + transferCosts[4][2][j - 1] + processingCosts[2][j])
+        {
+            f[2][j] = f[3][j - 1] + transferCosts[3][2][j - 1] + processingCosts[2][j];
+            l[2][j] = 4;
+        }
+        else
+        {
+            f[2][j] = f[4][j - 1] + transferCosts[4][2][j - 1] + processingCosts[2][j];
+            l[2][j] = 5;
+        }
+
+        // LINE = 4
+        if (f[3][j - 1] + processingCosts[3][j] <=
+            min(f[0][j - 1] + transferCosts[0][3][j - 1] + processingCosts[3][j],
+                min(f[1][j - 1] + transferCosts[1][3][j - 1] + processingCosts[3][j],
+                    min(f[2][j - 1] + transferCosts[2][3][j - 1] + processingCosts[3][j],
+                        f[4][j - 1] + transferCosts[4][3][j - 1] + processingCosts[3][j]))))
+        {
+            f[3][j] = f[3][j - 1] + processingCosts[3][j];
+            l[3][j] = 4;
+        }
+        else if (f[0][j - 1] + transferCosts[0][3][j - 1] + processingCosts[3][j] <=
+                 min(f[1][j - 1] + transferCosts[1][3][j - 1] + processingCosts[3][j],
+                     min(f[2][j - 1] + transferCosts[2][3][j - 1] + processingCosts[3][j],
+                         f[4][j - 1] + transferCosts[4][3][j - 1] + processingCosts[3][j])))
+        {
+            f[3][j] = f[0][j - 1] + transferCosts[0][3][j - 1] + processingCosts[3][j];
+            l[3][j] = 1;
+        }
+        else if (f[1][j - 1] + transferCosts[1][3][j - 1] + processingCosts[3][j] <=
+                 min(f[2][j - 1] + transferCosts[2][3][j - 1] + processingCosts[3][j],
+                     f[4][j - 1] + transferCosts[4][3][j - 1] + processingCosts[3][j]))
+        {
+            f[3][j] = f[1][j - 1] + transferCosts[1][3][j - 1] + processingCosts[3][j];
+            l[3][j] = 2;
+        }
+        else if (f[2][j - 1] + transferCosts[2][3][j - 1] + processingCosts[3][j] <=
+                 f[4][j - 1] + transferCosts[4][3][j - 1] + processingCosts[3][j])
+        {
+            f[3][j] = f[2][j - 1] + transferCosts[2][3][j - 1] + processingCosts[3][j];
+            l[3][j] = 3;
+        }
+        else
+        {
+            f[3][j] = f[4][j - 1] + transferCosts[4][3][j - 1] + processingCosts[3][j];
+            l[3][j] = 5;
+        }
+
+        // LINE = 5
+        if (f[4][j - 1] + processingCosts[4][j] <=
+            min(f[0][j - 1] + transferCosts[0][4][j - 1] + processingCosts[4][j],
+                min(f[1][j - 1] + transferCosts[1][4][j - 1] + processingCosts[4][j],
+                    min(f[2][j - 1] + transferCosts[2][4][j - 1] + processingCosts[4][j],
+                        f[3][j - 1] + transferCosts[3][4][j - 1] + processingCosts[4][j]))))
+        {
+            f[4][j] = f[4][j - 1] + processingCosts[4][j];
+            l[4][j] = 5;
+        }
+        else if (f[0][j - 1] + transferCosts[0][4][j - 1] + processingCosts[4][j] <=
+                 min(f[1][j - 1] + transferCosts[1][4][j - 1] + processingCosts[4][j],
+                     min(f[2][j - 1] + transferCosts[2][4][j - 1] + processingCosts[4][j],
+                         f[3][j - 1] + transferCosts[3][4][j - 1] + processingCosts[4][j])))
+        {
+            f[4][j] = f[0][j - 1] + transferCosts[0][4][j - 1] + processingCosts[4][j];
+            l[4][j] = 1;
+        }
+        else if (f[1][j - 1] + transferCosts[1][4][j - 1] + processingCosts[4][j],
+                 min(f[2][j - 1] + transferCosts[2][4][j - 1] + processingCosts[4][j],
+                     f[3][j - 1] + transferCosts[3][4][j - 1] + processingCosts[4][j]))
+        {
+            f[4][j] = f[1][j - 1] + transferCosts[1][4][j - 1] + processingCosts[4][j];
+            l[4][j] = 2;
+        }
+        else if (f[2][j - 1] + transferCosts[2][4][j - 1] + processingCosts[4][j] <=
+                 f[3][j - 1] + transferCosts[3][4][j - 1] + processingCosts[4][j])
+        {
+            f[4][j] = f[2][j - 1] + transferCosts[2][4][j - 1] + processingCosts[4][j];
+            l[4][j] = 3;
+        }
+        else
+        {
+            f[4][j] = f[3][j - 1] + transferCosts[3][4][j - 1] + processingCosts[4][j];
+            l[4][j] = 4;
+        }
+    }
+
+    if (f[0][JOBS - 1] + exitCosts[0] <=
+        min(f[1][JOBS - 1] + exitCosts[1],
+            min(f[2][JOBS - 1] + exitCosts[2],
+                f[3][JOBS - 1] + exitCosts[3])))
+    {
+        f[0][JOBS] = f[0][JOBS - 1] + exitCosts[0];
+        l[0][JOBS] = 1;
+    }
+    else if (f[1][JOBS - 1] + exitCosts[1] <=
+             min(f[2][JOBS - 1] + exitCosts[2],
+                 f[3][JOBS - 1] + exitCosts[3]))
+    {
+        f[1][JOBS] = f[1][JOBS - 1] + exitCosts[1];
+        l[1][JOBS] = 2;
+    }
+    else if (f[2][JOBS - 1] + exitCosts[2] <=
+             f[3][JOBS - 1] + exitCosts[3])
+    {
+        f[2][JOBS] = f[2][JOBS - 1] + exitCosts[2];
+        l[2][JOBS] = 3;
+    }
+    else
+    {
+        f[3][JOBS] = f[3][JOBS - 1] + exitCosts[3];
+        l[3][JOBS] = 4;
+    }
+
+    for (int i = 0; i < LINES; i++)
+    {
+        for (int j = 0; j <= JOBS; j++)
+        {
+            cout << f[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+    for (int i = 0; i < LINES; i++)
+    {
+        for (int j = 0; j <= JOBS; j++)
+        {
+            cout << l[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
 
-    /*int(*stations)[JOBS] = stationCost();
-    cout << stations << " " << *stations << endl;
+    /*int(*processingCosts)[JOBS] = stationCost();
+    cout << processingCosts << " " << *processingCosts << endl;
 
-    fastestWay(stations);*/
+    fastestWay(processingCosts);*/
 
     // csv_parsing();
     getEntryExitCosts();
     getProcessingCosts();
     getTransferCosts();
 
-    cout << "hello";
+    cout << endl;
+    cout << endl;
+    fastestWay();
 
     return 0;
 }
